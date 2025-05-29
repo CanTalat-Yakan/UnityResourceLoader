05ba28f284d8e20eafd587b1bc06617294e6470c
CanTalat-Yakan <57494543+CanTalat-Yakan@users.noreply.github.com>
2025-05-30 00:57:02 +0200

    ⠀⠀⠀⠀⠀

--- Editor/ResourceLoaderEditor.cs
+++ Editor/ResourceLoaderEditor.cs
@@ -6,19 +6,19 @@ namespace UnityEssentials
 {
     public static class ResourceLoaderEditor
     {
-        public static void InstantiatePrefab(string prefabName, string instantiatedName = null)
+        public static GameObject InstantiatePrefab(string prefabName, string instantiatedName = null)
         {
             if (string.IsNullOrWhiteSpace(prefabName))
             {
                 Debug.LogError("PrefabSpawnerEditor: prefabName is null or empty.");
-                return;
+                return null;
             }
 
             var guids = AssetDatabase.FindAssets(prefabName + " t:Prefab");
             if (guids.Length == 0)
             {
                 Debug.LogError($"{prefabName} prefab not found in project.");
-                return;
+                return null;
             }
             
             var path = AssetDatabase.GUIDToAssetPath(guids[0]);
@@ -26,7 +26,7 @@ namespace UnityEssentials
             if (prefab == null)
             {
                 Debug.LogError($"Failed to load prefab at: {path}");
-                return;
+                return null;
             }
 
             var instance = (GameObject)PrefabUtility.InstantiatePrefab(prefab);
@@ -36,8 +36,9 @@ namespace UnityEssentials
                 instance.name = instantiatedName;
 
             Undo.RegisterCreatedObjectUndo(instance, "Spawn " + instance.name);
-
             Selection.activeObject = instance;
+
+            return instance;
         }
     }
 }
