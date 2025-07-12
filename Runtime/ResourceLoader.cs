9ece3724e986c46cd8a5acf41206090688d713ce
CanTalat-Yakan <57494543+CanTalat-Yakan@users.noreply.github.com>
2025-07-12 21:22:07 +0200

    ⠀⠀⠀⠀⠀

--- Runtime/ResourceLoader.cs
+++ Runtime/ResourceLoader.cs
@@ -1,41 +1,44 @@
 using UnityEngine;
 
-public static class ResourceLoader
+namespace UnityEssentials
 {
-    public static T LoadResource<T>(string resourcePath) where T : Object
+    public static class ResourceLoader
     {
-        if (string.IsNullOrWhiteSpace(resourcePath))
+        public static T LoadResource<T>(string resourcePath) where T : Object
         {
-            Debug.LogError("ResourceLoader: resourcePath is null or empty.");
-            return null;
+            if (string.IsNullOrWhiteSpace(resourcePath))
+            {
+                Debug.LogError("ResourceLoader: resourcePath is null or empty.");
+                return null;
+            }
+
+            T resource = Resources.Load<T>(resourcePath);
+            if (resource == null)
+                Debug.LogError($"ResourceLoader: Could not find resource '{resourcePath}' in any Resources folder.");
+
+            return resource;
         }
 
-        T resource = Resources.Load<T>(resourcePath);
-        if (resource == null)
-            Debug.LogError($"ResourceLoader: Could not find resource '{resourcePath}' in any Resources folder.");
-
-        return resource;
-    }
-
-    public static GameObject InstantiatePrefab(string prefabName, string instantiatedName, Transform parent = null)
-    {
-        if (string.IsNullOrWhiteSpace(prefabName))
+        public static GameObject InstantiatePrefab(string prefabName, string instantiatedName, Transform parent = null)
         {
-            Debug.LogError("PrefabSpawner: prefabName is null or empty.");
-            return null;
+            if (string.IsNullOrWhiteSpace(prefabName))
+            {
+                Debug.LogError("PrefabSpawner: prefabName is null or empty.");
+                return null;
+            }
+
+            GameObject prefab = Resources.Load<GameObject>(prefabName);
+            if (prefab == null)
+            {
+                Debug.LogError($"PrefabSpawner: Could not find prefab '{prefabName}' in any Resources folder.");
+                return null;
+            }
+
+            var instance = Object.Instantiate(prefab, parent);
+            if (!string.IsNullOrEmpty(instantiatedName))
+                instance.name = instantiatedName;
+
+            return instance;
         }
-
-        GameObject prefab = Resources.Load<GameObject>(prefabName);
-        if (prefab == null)
-        {
-            Debug.LogError($"PrefabSpawner: Could not find prefab '{prefabName}' in any Resources folder.");
-            return null;
-        }
-
-        var instance = Object.Instantiate(prefab, parent);
-        if (!string.IsNullOrEmpty(instantiatedName))
-            instance.name = instantiatedName;
-
-        return instance;
     }
 }
